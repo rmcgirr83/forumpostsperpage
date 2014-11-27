ed4629768541eb969c15061ff8fcef4133657576
Richard McGirr <rmcgirr83@gmail.com>
2014-11-27 08:48:13 -0500

    Add viewtopic and caching of forum entries

--- event/listener.php
+++ event/listener.php
@@ -20,6 +20,9 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class listener implements EventSubscriberInterface
 {
 
+	/** @var \phpbb\cache\driver\driver_interface */
+	protected $cache;
+
 	/** @var \phpbb\config\config */
 	protected $config;
 
@@ -29,25 +32,33 @@ class listener implements EventSubscriberInterface
 	/** @var \phpbb\request\request */
 	protected $request;
 
-	public function __construct(\phpbb\config\config $config, \phpbb\db\driver\driver_interface $db, \phpbb\request\request $request)
+	/** @var \phpbb\user */
+	protected $user;
+
+	public function __construct(\phpbb\cache\service $cache, \phpbb\config\config $config, \phpbb\db\driver\driver_interface $db, \phpbb\request\request $request, \phpbb\user $user)
 	{
+		$this->cache = $cache;
 		$this->config = $config;
 		$this->db = $db;
 		$this->request = $request;
+		$this->user = $user;
 	}
 
 	static public function getSubscribedEvents()
 	{
 		return array(
+			// ACP activities
 			'core.acp_manage_forums_request_data'		=> 'acp_manage_forums_request_data',
 			'core.acp_manage_forums_initialise_data'	=> 'acp_manage_forums_initialise_data',
 			'core.acp_manage_forums_display_form'		=> 'acp_manage_forums_display_form',
 			'core.acp_manage_forums_validate_data'		=> 'acp_manage_forums_validate_data',
+			'core.acp_manage_forums_update_data_after'	=> 'acp_manage_forums_update_data_after',
+			// forum changes to config[posts_per_page]
 			'core.viewforum_get_topic_data'				=> 'viewforum_get_topic_data',
-			/*'core.viewtopic_modify_post_data'			=> 'viewtopic_modify_post_data',*/
 			'core.display_forums_modify_template_vars'	=> 'display_forums_modify_template_vars',
 			'core.search_modify_rowset'					=> 'search_modify_rowset',
 			'core.modify_mcp_modules_display_option'	=> 'modify_mcp_modules_display_option',
+			'core.user_setup'							=> 'user_setup',
 		);
 	}
 
@@ -89,31 +100,27 @@ class listener implements EventSubscriberInterface
 		$event['errors'] = $errors;
 	}
 
+	// purge our cache after forums are updated
+	public function acp_manage_forums_update_data_after($event)
+	{
+		$this->cache->purge('_forum_posts_per_page');
+	}
+
 	// modify viewforum and reset config['posts_per_page']
 	public function viewforum_get_topic_data($event)
 	{
 		if (!empty($event['forum_data']['forum_posts_per_page']))
 		{
-			$this->config->offsetSet('posts_per_page', $event['forum_data']['forum_posts_per_page']);
+			$this->posts_per_page($event['forum_data']['forum_posts_per_page']);
 		}
 	}
 
-	// TODO need to find a way to reset config on viewtopic without hard coding...the below does not work
-	// modify viewtopic and reset config['posts_per_page']
-/*	public function viewtopic_modify_post_data($event)
-	{
-		if (!empty($event['topic_data']['forum_posts_per_page']))
-		{
-			$this->config->offsetSet('posts_per_page', $event['topic_data']['forum_posts_per_page']);
-		}
-	}
-*/
 	// modify functions display and reset config['posts_per_page']
 	public function display_forums_modify_template_vars($event)
 	{
 		if (!empty($event['row']['forum_posts_per_page']))
 		{
-			$this->config->offsetSet('posts_per_page', $event['row']['forum_posts_per_page']);
+			$this->posts_per_page($event['row']['forum_posts_per_page']);
 		}
 	}
 
@@ -126,7 +133,7 @@ class listener implements EventSubscriberInterface
 			$posts_per_page = $this->get_forum_data($row['forum_id']);
 			if (!empty($posts_per_page))
 			{
-				$this->config->offsetSet('posts_per_page', $posts_per_page);
+				$this->posts_per_page($posts_per_page);
 			}
 		}
 	}
@@ -134,26 +141,127 @@ class listener implements EventSubscriberInterface
 	// modify and reset mcp
 	public function modify_mcp_modules_display_option($event)
 	{
-		if ($event['mode'] == 'topic_view')
+		if ($event['mode'] == 'topic_view' || $event['mode'] == 'forum_view')
 		{
 			$posts_per_page = $this->get_forum_data($event['forum_id']);
 			if (!empty($posts_per_page))
 			{
-				$this->config->offsetSet('posts_per_page', $posts_per_page);
+				$this->posts_per_page($posts_per_page);
 			}
 		}
 	}
 
-	// get forum posts per page and return value
+	// check what page the user is on and the query string as well
+	public function user_setup($event)
+	{
+		$page_name = substr($this->user->page['page_name'], 0, strpos($this->user->page['page_name'], '.'));
+
+		//we only care about viewtopic all others are handled by events
+		if ($page_name == 'viewtopic')
+		{
+			$string = $this->user->page['query_string'];
+			$string = parse_str($string, $output);
+			foreach ($output as $key => $value)
+			{
+				if ($key == 'p')
+				{
+					$posts_per_page = $this->get_forum_id_from_posts_table($value);
+					if (!empty($posts_per_page))
+					{
+						$this->posts_per_page($posts_per_page);
+					}
+				}
+				if ($key == 'f')
+				{
+					$posts_per_page = $this->get_forum_data($value);
+					if (!empty($posts_per_page))
+					{
+						$this->posts_per_page($posts_per_page);
+					}
+				}
+				if ($key == 't')
+				{
+					$posts_per_page = $this->get_forum_id_from_topics_table($value);
+					if (!empty($posts_per_page))
+					{
+						$this->posts_per_page($posts_per_page);
+					}
+				}
+			}
+		}
+	}
+
+	// change posts_per_page
+	private function posts_per_page($data)
+	{
+		$this->config->offsetSet('posts_per_page', $data);
+	}
+
+	// need to retrieve the forum_id from the posts table
+	private function get_forum_id_from_posts_table($post_id)
+	{
+		$sql = 'SELECT forum_id
+			FROM ' . POSTS_TABLE . '
+			WHERE post_id = ' . $post_id;
+		$result = $this->db->sql_query($sql);
+		$temp = $this->db->sql_fetchrow($result);
+		$this->db->sql_freeresult($result);
+		
+		// now check the cache for an entry
+		$temp = $this->get_forum_data($temp['forum_id']);
+		return $temp;
+	}
+
+	// need to retrieve the forum_id from the posts table
+	private function get_forum_id_from_topics_table($topic_id)
+	{
+		$sql = 'SELECT forum_id
+			FROM ' . TOPICS_TABLE . '
+			WHERE topic_id = ' . $topic_id;
+		$result = $this->db->sql_query($sql);
+		$temp = $this->db->sql_fetchrow($result);
+		$this->db->sql_freeresult($result);
+		
+		// now check the cache for an entry
+		$temp = $this->get_forum_data($temp['forum_id']);
+		return $temp;
+	}
+
+	// get forum posts per page and set the cache 
 	private function get_forum_data($forum_id)
 	{
-		$sql = 'SELECT forum_posts_per_page
-				FROM ' . FORUMS_TABLE . '
-				WHERE forum_id = ' . (int) $forum_id;
-		$result = $this->db->sql_query($sql);
-		$posts_per_page = $this->db->sql_fetchrow($result);
-		$this->db->sql_freeresult($result);
+		if (($posts_per_page = $this->cache->get('_forum_posts_per_page')) === false)
+		{	
+			// we only want those forums that you can post in
+			$sql = 'SELECT forum_posts_per_page, forum_id
+					FROM ' . FORUMS_TABLE . '
+					WHERE forum_type = ' . FORUM_POST . '
+					ORDER BY forum_id DESC';
+			$result = $this->db->sql_query($sql);
+			while ($row = $this->db->sql_fetchrow($result))
+			{
+				$posts_per_page[$row['forum_id']] = array(
+					'forum_id'			=> $row['forum_id'],
+					'forum_posts_per_page'	=> $row['forum_posts_per_page'],
+				);
+			}
+			$this->db->sql_freeresult($result);
 
-		return $posts_per_page['forum_posts_per_page'];
+			// cache this data for ever, can only change in ACP
+			$this->cache->put('_forum_posts_per_page', $posts_per_page);
+		}
+
+		$fpp = 0;
+		if (sizeof($posts_per_page))
+		{
+			foreach ($posts_per_page as $row)
+			{
+				if ($row['forum_id'] == $forum_id)
+				{
+					$fpp = $row['forum_posts_per_page'];
+				}
+			}
+		}
+		return $fpp;
 	}
 }
